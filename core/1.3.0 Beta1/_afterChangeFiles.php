6723f10ad6972248ed23e05877501ae3d21a3351
Max Koder <mx.koder@gmail.com>
2023-09-16 22:07:38 +0200

    Fix 1.3.0 Beta1

--- core/1.3.0 Beta1/_afterChangeFiles.php
+++ core/1.3.0 Beta1/_afterChangeFiles.php
@@ -3,7 +3,7 @@
 logg("Began of _afterChangeFiles", "INFO");
 
 // After SEO change config file
-$pluginsManager->installPlugin('seo', true);
+pluginsManager::getInstance()->installPlugin('seo', true);
 
 logg("End of _afterChangeFiles", "INFO");
 
